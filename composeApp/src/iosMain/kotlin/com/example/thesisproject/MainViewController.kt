c99ce8625e90973f3b65f1ef73ebac6d3a74b772
Amit Kundu <kundu.amit517@gmail.com>
2026-01-18 10:13:14 +0200

    feat: fix some deprecations and IOS build issue

--- composeApp/src/iosMain/kotlin/com/example/thesisproject/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/example/thesisproject/MainViewController.kt
@@ -2,5 +2,25 @@ package com.example.thesisproject
 
 import androidx.compose.ui.window.ComposeUIViewController
 import com.amit.newsreader.App
+import com.amit.newsreader.di.initKoin
+import com.amit.newsreader.di.viewModelModule
 
-fun MainViewController() = ComposeUIViewController { App() }
+/**
+ * Lazy initialization of Koin - initialized once on first access
+ * This is thread-safe and idiomatic Kotlin
+ */
+private val koinInitializer by lazy {
+    initKoin {
+        modules(viewModelModule)
+    }
+}
+
+/**
+ * Entry point for iOS application
+ * Returns a UIViewController that hosts the Compose UI
+ */
+fun MainViewController() = ComposeUIViewController {
+    // Trigger lazy initialization of Koin
+    koinInitializer
+    App()
+}
